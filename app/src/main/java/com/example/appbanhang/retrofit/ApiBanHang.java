07836b70084501d0b8ba419a48796dea494d5a5d
nguyenducquanglinh <linh35889@gmail.com>
2024-09-15 23:21:23 +0700

    ban hang project

--- app/src/main/java/com/example/appbanhang/retrofit/ApiBanHang.java
+++ app/src/main/java/com/example/appbanhang/retrofit/ApiBanHang.java
@@ -6,6 +6,6 @@ import io.reactivex.rxjava3.core.Observable;
 import retrofit2.http.GET;
 
 public interface ApiBanHang {
-    @GET("loaisanpham")
+    @GET("getloaisp.php")
     Observable<LoaiSpModel> getLoaiSp();
 }
